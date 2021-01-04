1ef53cb66b5591f0afa42fd857db7e95b4b8c5d7
Dejan Milicic <dejan.milicic@gmail.com>
2021-01-04 22:03:26 +0100

    Seeding data

---
+++ src/RavenCms/RavenCms/Controllers/EntryController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using Raven.Client.Documents;
+using Raven.Client.Documents.BulkInsert;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Documents.Session;
+using RavenCms.Infrastructure;
+using RavenCms.Models;
+
+namespace RavenCms.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EntryController : ControllerBase
+    {
+        private readonly IAsyncDocumentSession _session;
+        private readonly IDocumentStore _store;
+
+        public EntryController(IAsyncDocumentSession session, IDocumentStore store)
+        {
+            _session = session;
+            _store = store;
+        }
+
+        [HttpGet("/seed")]
+        public async Task<string> Seed()
+        {
+            DetailedDatabaseStatistics stats = _store.Maintenance.Send(new GetDetailedStatisticsOperation());
+            if (stats.CountOfDocuments > 0)
+                return "Database is already seeded";
+
+            Faker<Entry> generator = new Faker<Entry>()
+                .StrictMode(true)
+                .Ignore(e => e.Id)
+                .RuleFor(e => e.Tags, f => Helper.GetRandomTags());
+
+            List<Entry> entries = generator.Generate(20);
+
+            await using BulkInsertOperation bulkInsert = _store.BulkInsert();
+            foreach (Entry entry in entries)
+            {
+                await bulkInsert.StoreAsync(entry);
+            }
+
+            return "Database was empty, new data seeded";
+        }
+    }
+}
---
+++ src/RavenCms/RavenCms/Models/Entry.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace RavenCms.Models
+{
+    public class Entry
+    {
+        public string Id { get; set; }
+
+        public List<string> Tags { get; set; }
+    }
+}
