0fd3ed4c3e937e297e91cabb7f8ca1fc4acc1a6b
Dejan Milicic <dejan.milicic@gmail.com>
2021-01-07 13:49:20 +0100

    load test

--- src/RavenCms/RavenCms.LoadTesting/Program.cs
+++ src/RavenCms/RavenCms.LoadTesting/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Threading.Tasks;
+using NBomber;
 using NBomber.Contracts;
 using NBomber.CSharp;
 using NBomber.Plugins.Http.CSharp;
 using NBomber.Plugins.Network.Ping;
-using Newtonsoft.Json;
 
 namespace RavenCms.LoadTesting
 {
@@ -12,52 +11,44 @@ namespace RavenCms.LoadTesting
     {
         static void Main(string[] args)
         {
-            /*
-            // first, you need to create a step
-            var stepEmpty = Step.Create("step", async context =>
-            {
-                // you can define and execute any logic here,
-                // for example: send http request, SQL query etc
-                // NBomber will measure how much time it takes to execute your logic
-
-                await Task.Delay(TimeSpan.FromSeconds(1));
-                return Response.Ok();
-            });
-
-            // second, we add our step to the scenario
-            var scenario = ScenarioBuilder.CreateScenario("hello_world", stepEmpty);
-
-            NBomberRunner
-                .RegisterScenarios(scenario)
-                .Run();
-            */
-
-            var healthCheck = HttpStep.Create("healthcheck", context =>
-                Http.CreateRequest("GET", "http://localhost:52788/healthcheck")
-                    .WithCheck(async response =>
-                    {
-                        var rc = await response.Content.ReadAsStringAsync();
-
-                        return rc == "Healthy"
-                            ? Response.Ok()
-                            : Response.Fail();
-                    })
-            );
-
-            var scenario = ScenarioBuilder
-                .CreateScenario("smoke test", healthCheck)
-                .WithLoadSimulations(new[]
-                {
-                    Simulation.InjectPerSec(rate: 100, during: TimeSpan.FromSeconds(30))
-                });
-
-            var pingPluginConfig = PingPluginConfig.CreateDefault(new[] {"RavenCms"});
+            var pingPluginConfig = PingPluginConfig.CreateDefault(new[] { "RavenCms" });
             var pingPlugin = new PingPlugin(pingPluginConfig);
 
             NBomberRunner
-                .RegisterScenarios(scenario)
+                .RegisterScenarios(GetEntriesByTag())
                 .WithWorkerPlugins(pingPlugin)
                 .Run();
+        }
+
+        public static Scenario GetEntriesByTag()
+        {
+            var data = FeedData.FromJson<string>("tags.json");
+            var tagFeed = Feed.CreateCircular("tagFeed", provider: data);
+
+            var step = HttpStep.Create("step", tagFeed, context =>
+            {
+                string url = "http://localhost:5000/" + context.FeedItem;
+
+                context.Logger.Debug("Data from feed: {FeedItem}", context.FeedItem);
+
+                return Http.CreateRequest("GET", url)
+                    .WithCheck(async response =>
+                        response.IsSuccessStatusCode
+                            ? Response.Ok()
+                            : Response.Fail()
+                    );
+            });
+
+            var scenario = ScenarioBuilder
+                .CreateScenario("GetEntriesByTag", step)
+                .WithoutWarmUp()
+                .WithLoadSimulations(new[]
+                {
+                    Simulation
+                        .InjectPerSec(rate: 1_000, during: TimeSpan.FromSeconds(20))
+                });
+
+            return scenario;
         }
     }
 }
