d6dd482f0e1c0c5f15a6024bdf122fa0a7bf2348
Dejan Milicic <dejan.milicic@gmail.com>
2021-01-09 14:47:28 +0100

    models

---
+++ src/RavenCms/RavenCms/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RavenCms.Models
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public List<string> Notes { get; set; }
+        
+        public string ReportsTo { get; set; }
+        
+        public string Title { get; set; }
+    }
+}
---
+++ src/RavenCms/RavenCms/Models/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RavenCms.Models
+{
+    public class Order
+    {
+        public string Id { get; set; }
+
+        public string Company { get; set; }
+
+        public string Employee { get; set; }
+        
+        public float Freight { get; set; }
+        
+        public List<Line> Lines { get; set; }
+        
+        public DateTimeOffset OrderedAt { get; set; }
+        
+        public DateTimeOffset RequireAt { get; set; }
+        
+        public string ShipVia { get; set; }
+
+
+        public class Line
+        {
+            public float Discount { get; set; }
+
+            public float PricePerUnit { get; set; }
+            
+            public string Product { get; set; }
+            
+            public string ProductName { get; set; }
+            
+            public int Quantity { get; set; }
+        }
+    }
+}
